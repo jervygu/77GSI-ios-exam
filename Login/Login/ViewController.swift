2c59281ce35578d3cfdf6fd6e6c0c9c91643cea3
Jervy Umandap <jervy.umandap@whitecloak.com>
2025-10-15 21:06:53 +0800

    - Login Screen layout

--- Login/Login/ViewController.swift
+++ Login/Login/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var usernameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var loginBtn: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func loginTapped(_ sender: UIButton) {
+        
+        print("loginTapped")
+    }
+    
 
 }
 
